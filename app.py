728be28d8925ed9af6c699c1170db0165b1358f1
H95u <hieu.dh3t1@gmail.com>
2023-10-24 16:11:49 +0700

    update controller

--- app.py
+++ app.py
@@ -2,17 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
-@app.route('/home')
-def home():  # put application's code here
-    return 'Home!'
-
-
+try:
+    from controller import *
+except Exception as e:
+    print(e)
 
 if __name__ == '__main__':
     app.run()
